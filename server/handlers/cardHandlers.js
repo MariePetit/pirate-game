ba9ff5619a342c5bd29af516ba24c03d3e630f60
Nicolas Beaulieu-Gagnon <beaulieu_gagnon_nicolas@hotmail.com>
2021-07-14 16:02:07 -0400

    adding some context and the game works very barebones

--- server/handlers/cardHandlers.js
+++ server/handlers/cardHandlers.js
@@ -1,13 +1,21 @@
+const { v4: uuidv4 } = require("uuid");
+
 const { eventCards, endCards } = require("../Cards");
 
 const getAllCards = (req, res) => {
-  res
-    .status(200)
-    .json({
-      status: 200,
-      data: { eventCards, endCards },
-      message: "grabbed all cards",
-    });
+  const newEventCards = eventCards.map((card) => {
+    return { ...card, id: uuidv4() };
+  });
+
+  const newEndCards = endCards.map((card) => {
+    return { ...card, id: uuidv4() };
+  });
+
+  res.status(200).json({
+    status: 200,
+    data: { eventCards: newEventCards, endCards: newEndCards },
+    message: "grabbed all cards",
+  });
 };
 
 module.exports = { getAllCards };
