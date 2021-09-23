1c22b4be632cf39e2a3d0a1cacc1af7fd7c013b4
Nicolas Beaulieu-Gagnon <beaulieu_gagnon_nicolas@hotmail.com>
2021-09-22 20:59:38 -0400

    added editable profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useContext } from "react";
+import styled from "styled-components";
+
+import { UserContext } from "../components/UserContext";
+import UserEdit from "../components/settingComponents/UserEdit";
 
 const Profile = () => {
-  return <div></div>;
+  const {
+    user: {
+      userName,
+      email,
+      avatarSrc,
+      password,
+      joined,
+      firstName,
+      lastName,
+      _id,
+    },
+    setUser,
+  } = useContext(UserContext);
+  const arrayOfUserInfo = [
+    { email },
+    { password },
+    { firstName },
+    { lastName },
+  ];
+
+  const handleEditFunction = (
+    oldValue,
+    newValue,
+    infoKey,
+    handleCloseEdit,
+    setMakingChanges
+  ) => {
+    if (newValue !== oldValue) {
+      let value = {};
+      value[infoKey] = newValue;
+
+      fetch(`/user/${_id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(value),
+      }).then(() => {
+        fetch(`/user/${_id}`).then((res) => {
+          res
+            .json()
+            .then(({ data }) => {
+              setUser(data);
+              setMakingChanges(false);
+            })
+            .then(() => {
+              handleCloseEdit();
+            });
+        });
+      });
+    } else {
+      handleCloseEdit();
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Profile</Title>
+      {arrayOfUserInfo.map((info, index) => {
+        return (
+          <UserEdit
+            index={index}
+            key={index}
+            infoValue={Object.values(info)[0]}
+            infoKey={Object.keys(info)[0]}
+            handleEditFunction={handleEditFunction}
+          ></UserEdit>
+        );
+      })}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div``;
+
+const Title = styled.div``;
 
 export default Profile;
